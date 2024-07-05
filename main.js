cf3a91465c7942337adba8d1054c50f5f189118a
Minjoo Kim <161261407+MinjooKim1@users.noreply.github.com>
2024-07-05 01:26:55 -0700

    seventh commit

--- main.js
+++ main.js
@@ -2,30 +2,23 @@ let taskInput = document.getElementById("task-input");
 let plusBtn = document.getElementById("plus-btn");
 let taskList = [];
 let tabs = document.querySelectorAll(".task-taps div");
+let underLine = document.getElementById("under-line");
 let mode = "all";
 let filterList = [];
-let underLine = document.getElementById("under-line");
 
 plusBtn.addEventListener("click", addList);
 
-taskInput.addEventListener("keypress", function (e) {
-  if (e.key === "Enter") {
-    e.preventDefault();
+taskInput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
     document.getElementById("plus-btn").click();
   }
 });
 
 for (let i = 1; i < tabs.length; i++) {
-  tabs[i].addEventListener("click", function (event) {
-    filter(event);
+  tabs[i].addEventListener("click", function (e) {
+    filter(e);
   });
-  tabs.forEach((menu) => menu.addEventListener("click", (e) => indicator(e)));
-}
-function indicator(e) {
-  underLine.style.left = e.currentTarget.offsetLeft + "px";
-  underLine.style.width = e.currentTarget.offsetWidth + "px";
-  underLine.style.top =
-    e.currentTarget.offsetTop + e.currentTarget.offsetHeight + "px";
 }
 
 function addList() {
@@ -43,14 +36,13 @@ function addList() {
 }
 
 function render() {
+  let resultHTML = "";
   let list = [];
   if (mode === "all") {
     list = taskList;
   } else if (mode === "ongoing" || mode === "done") {
     list = filterList;
   }
-
-  let resultHTML = "";
   for (let i = 0; i < list.length; i++) {
     if (list[i].isComplete == true) {
       resultHTML += `<div class="tasks">
@@ -89,8 +81,7 @@ function toggle(id) {
       break;
     }
   }
-  render();
-  console.log(taskList);
+  filter();
 }
 
 function deleteTask(id) {
@@ -100,31 +91,34 @@ function deleteTask(id) {
       break;
     }
   }
-  render();
-  console.log(taskList);
+
+  filter();
 }
 
-function filter(event) {
-  mode = event.target.id;
+function filter(e) {
+  if (e) {
+    mode = e.target.id;
+    underLine.style.width = e.target.offsetWidth + "px";
+    underLine.style.left = e.target.offsetLeft + "px";
+    underLine.style.top =
+      e.target.offsetTop + (e.target.offsetHeight - 4) + "px";
+  }
+
   filterList = [];
-  if (mode === "all") {
-    render();
-  } else if (mode === "ongoing") {
+  if (mode === "ongoing") {
     for (let i = 0; i < taskList.length; i++) {
-      if (taskList[i].isComplete === false) {
+      if (taskList[i].isComplete == false) {
         filterList.push(taskList[i]);
       }
     }
-    render();
-    console.log("ongoing", filterList);
   } else if (mode === "done") {
     for (let i = 0; i < taskList.length; i++) {
-      if (taskList[i].isComplete === true) {
+      if (taskList[i].isComplete) {
         filterList.push(taskList[i]);
       }
     }
-    render();
   }
+  render();
 }
 
 function generateID() {
